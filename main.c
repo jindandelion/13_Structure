e418d8f337b377b4f87d8cc5d0ef1601e6b0d026
jindandelion <gieoun524@sookmyung.ac.kr>
2019-11-28 16:10:19 +0900

    Practice2

--- main.c
+++ main.c
@@ -1,23 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-struct student{
-	int ID;
-	char name[10];
-	double grade;
-	};
- 
-int main(int argc, char *argv[]) {
-	struct student s1={24,"JIUN",4.3};
-	s1.ID=1911610;
-	strcpy(s1.name,"JIUN");
-	s1.grade=4.3;
-	
-	printf("ID : %d\n",s1.ID);
-	printf("name : %s\n", s1.name);
-	printf("grade : %f\n",s1.grade);
+int is_whitespace(char c)
+{
+	if(c == ' '||
+	   c == '\n'||
+	   c == '\r'||
+	   c == '\t'||
+	   c == '('
+	   )
+	{
+		return 1;
+	}
 	
 	return 0;
+ } 
+ 
+int fget_word(FILE* fp,char *word)
+{
+	int cnt = 0; 
+	char c;
+	
+	while((c=fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(c)==0)
+			break;
+	}
+	
+	if(c == EOF)
+	{
+		return 0;
+	}
+	
+	cnt =0;
+	word[cnt++] = c;
+	word[cnt] = '\0';//한글자로 끝날 수도 있으니까. 
+
+	while((word[cnt]= fgetc(fp))!=EOF)
+	{
+		if(is_whitespace(word[cnt]) == 1)
+		{
+			word[cnt]='\0';//End of string
+			break;
+		}
+		cnt++;
+	}
+	
+	return cnt;//이제 읽은 문장의 글자수가 몇인지 뱉어주는 것임. 
+	
 }
+int main(int argc, char *argv[]) {
+	
+	FILE *fp;
+	char filepath[100];
+	char word[100];
+	
+	//file open
+	printf("input the file path: ");
+	scanf("%s",filepath); 
+	fp = fopen(filepath,"r");
+	
+	if(fp == NULL)//fopen했는데도 NULL이면 잘못됬다고 출력해줌.
+	{
+		printf("file path is wrong! %s\n",filepath);
+		return -1; //잘못입력됬을 때 밑으로 프로그램이 못가게 해주는 것. 
+	} 
+
+	
+	//word
+	while(fget_word(fp,word)!=0)
+	{
+		printf("%s\n",word);
+	}
+	fclose(fp);
+	
+}
+
  
